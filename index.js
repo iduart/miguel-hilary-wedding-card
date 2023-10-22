aeef2c9bf23a03a20ef34f1cfa3dc1a6c6c5bebb
Iduart <iduartdean@gmail.com>
2023-10-22 01:00:22 -0500

    as

--- index.js
+++ index.js
@@ -18,4 +18,36 @@ document.addEventListener("DOMContentLoaded", function () {
   sections.forEach((section) => {
     revealObserver.observe(section);
   });
+
+  // Slider
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".slider-image");
+  const dots = document.querySelectorAll(".dot");
+
+  function showSlide(n) {
+    slides[currentSlide].style.display = "none";
+    dots[currentSlide].classList.remove("active");
+
+    currentSlide = (n + slides.length) % slides.length;
+
+    slides[currentSlide].style.display = "block";
+    dots[currentSlide].classList.add("active");
+  }
+
+  function nextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  // Show the first slide initially
+  showSlide(currentSlide);
+
+  // Autoplay functionality
+  const slideInterval = setInterval(nextSlide, 3000); // Change 3000ms (3 seconds) to your desired autoplay interval
+
+  // Dot navigation functionality
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      showSlide(index);
+    });
+  });
 });
